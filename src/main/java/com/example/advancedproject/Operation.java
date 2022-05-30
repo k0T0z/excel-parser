b994228f953d1880d256722d57d3163e68c2eae2
saifkandil <74428638+saifkandil@users.noreply.github.com>
2022-05-30 15:50:26 +0200

    Delete src/main directory

--- src/main/java/com/example/advancedproject/Operation.java
+++
@@ -1,6 +0,0 @@
-package com.example.advancedproject;
-
-public class Operation {
-    char responseType;
-
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.advancedproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.advancedproject to javafx.fxml;
-    exports com.example.advancedproject;
-}
